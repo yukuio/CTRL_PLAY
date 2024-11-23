736bb9b482f61ae1f6be4459fe92134b322d429a
Josue-Quisbert <matheusyukio695@gmail.com>
2024-11-23 15:05:49 -0400

    ManipularDados.py

---
+++ ExercicioManupilarDados.py
@@ -0,0 +1,6 @@
+n1 = float(input("qual a primeira nota:"))
+n2 = float(input("qual a segunda nota:"))
+
+media = (n1+n2)/2
+
+print("o usuario teve como media:",media)
---
+++ ManipularDados.py
@@ -0,0 +1,19 @@
+# nome = "matheus"
+# sobrenome = "matheusyukio695@gmail.com"
+
+# print(nome + sobrenome)
+
+# print("matheus \nyukio \neh \naluno")
+
+# print(len(sobrenome))
+
+# print(sobrenome.find("@"))
+
+# print(sobrenome.count("."))
+
+email = input("escreva seu email:")
+
+print(email)
+print("quantidade de caracteres no email:", len(email))
+
+
